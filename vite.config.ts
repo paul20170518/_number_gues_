98976423ce25e642fdee5e6fb824ce0df5c3cba8
潘清輝 <haiupan@gmail.com>
2026-05-19 15:47:44 +0800

    修改2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-  base: '/_number_gues_/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
